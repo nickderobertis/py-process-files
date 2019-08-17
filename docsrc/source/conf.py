536647a8a23dcd2502a81e0cfd0ee9a9cf33620d
Nick DeRobertis <nick@claimfound.com>
2019-08-17 17:18:59 -0400

    0.1.2: Update docs and add __init__ methods to docs

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -189,3 +189,11 @@ texinfo_documents = [
 ]
 
 
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
