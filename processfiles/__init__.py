4de939b8b9f9f0603bf8579c9366df16611646d3
Nick DeRobertis <whoopnip@gmail.com>
2019-10-01 19:43:41 -0400

    Add docstring

--- processfiles/__init__.py
+++ processfiles/__init__.py
@@ -1,4 +1,9 @@
-
+"""
+Use this tool to select files of given file types in a folder, and track whether these files have been processed,
+regardless of whether the script needs to be run multiple times. Stores progress on the files as a text file in the
+same folder, so that a long-running operation on many files can be resumed where it left off if it was stopped.
+It will also automatically estimate time to completion.
+"""
 
 
 
