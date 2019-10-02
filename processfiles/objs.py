e2783f706450694d0bcbfe517e08df7de6570d3d
Nick DeRobertis <whoopnip@gmail.com>
2019-10-01 20:11:50 -0400

    Add chunking functionality to object process tracker

--- processfiles/objs.py
+++ processfiles/objs.py
@@ -1,7 +1,9 @@
-from typing import Iterable
+from typing import Iterable, Optional
+import math
 
 from processfiles.timing import TimeTracker
 from processfiles.base import BaseProcessTracker
+import itertools
 
 
 class ObjectProcessTracker(BaseProcessTracker):
@@ -16,19 +18,41 @@ class ObjectProcessTracker(BaseProcessTracker):
 
         self.load_completed_files()
 
-    def obj_generator(self):
+    def obj_generator(self, chunk: Optional[int] = None):
         timer = TimeTracker(None, restart=self.restart)
         num_items = len(self.objs)
 
-        for obj in self.objs:
-            if self._has_been_completed(obj):
-                continue
-            yield obj
-            self.add_to_completed(obj)
-            timer.time_estimate(num_items)
+        if chunk:
+            num_items = math.ceil(len(self.objs) / chunk)
+            for objs_chunk in chunk_generator(chunk, self.objs):
+                valid_chunk = []
+                for obj in objs_chunk:
+                    if not self._has_been_completed(obj):
+                        valid_chunk.append(obj)
+                if not valid_chunk:
+                    continue
+                yield valid_chunk
+                [self.add_to_completed(obj) for obj in valid_chunk]
+                timer.time_estimate(num_items)
+        else:
+            for obj in self.objs:
+                if self._has_been_completed(obj):
+                    continue
+                yield obj
+                self.add_to_completed(obj)
+                timer.time_estimate(num_items)
 
         # time_estimate is end \r, so this cancels the next output from writing over the final time estimate
         print('\n')
 
     def _has_been_completed(self, obj):
         return str(obj) in self.completed_list
+
+
+def chunk_generator(n, iterable):
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, n))
+        if not chunk:
+            return
+        yield chunk
